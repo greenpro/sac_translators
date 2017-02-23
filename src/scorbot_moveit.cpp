ad640c15c7aa45af73b17ff4ae059e55370a439c
Craig Christensen <crg.chrstnsn@gmail.com>
2017-02-23 10:37:19 -0600

    Addition of files to allow for various configs and changes to the ik translator.

--- src/scorbot_moveit.cpp
+++ src/scorbot_moveit.cpp
@@ -43,7 +43,7 @@
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
 
-//#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 //<<<<<<< HEAD
 
 #define NODE_NAME "scorbot_moveit"
@@ -73,115 +73,118 @@ int main(int argc, char **argv)
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
   ROS_INFO("loaded arm");
 
-  //// We will use the :planning_scene_interface:`PlanningSceneInterface`
-  //// class to add and remove collision objects in our "virtual world" scene
-  //moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  // We will use the :planning_scene_interface:`PlanningSceneInterface`
+  // class to add and remove collision objects in our "virtual world" scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-  //// Raw pointers are frequently used to refer to the planning group for improved performance.
-  //const robot_state::JointModelGroup *joint_model_group =
-  //  move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+  // Raw pointers are frequently used to refer to the planning group for improved performance.
+  const robot_state::JointModelGroup *joint_model_group =
+    move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  //// Visualization
-  //// ^^^^^^^^^^^^^
-  ////
-  //// The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
-  //// and trajectories in Rviz as well as debugging tools such as step-by-step introspection of a script
-  ////namespace rvt = rviz_visual_tools;
-  ////moveit_visual_tools::MoveItVisualTools visual_tools("odom_combined");
-  ////visual_tools.deleteAllMarkers();
+  // Visualization
+  // ^^^^^^^^^^^^^
+  //
+  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
+  // and trajectories in Rviz as well as debugging tools such as step-by-step introspection of a script
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("odom_combined");
+  visual_tools.deleteAllMarkers();
 
-  //// Remote control is an introspection tool that allows users to step through a high level script
-  //// via buttons and keyboard shortcuts in Rviz
-  ////visual_tools.loadRemoteControl();
+  // Remote control is an introspection tool that allows users to step through a high level script
+  // via buttons and keyboard shortcuts in Rviz
+  visual_tools.loadRemoteControl();
 
-  //// Rviz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  //Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-  //text_pose.translation().z() = 1.75; // above head of PR2
-  ////visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+  // Rviz provides many types of markers, in this demo we will use text, cylinders, and spheres
+  ROS_INFO("98");
+  Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+  text_pose.translation().z() = 1.75; // above head of PR2
+  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
 
-  //// Batch publishing is used to reduce the number of messages being sent to Rviz for large visualizations
-  ////visual_tools.trigger();
+  // Batch publishing is used to reduce the number of messages being sent to Rviz for large visualizations
+  ROS_INFO("104");
+  visual_tools.trigger();
 
-  //// Getting Basic Information
-  //// ^^^^^^^^^^^^^^^^^^^^^^^^^
-  ////
-  //// We can print the name of the reference frame for this robot.
-  //ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+  // Getting Basic Information
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // We can print the name of the reference frame for this robot.
+  ROS_INFO_NAMED("tutorial", "111 Reference frame: %s", move_group.getPlanningFrame().c_str());
 
-  //// We can also print the name of the end-effector link for this group.
-  //ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+  // We can also print the name of the end-effector link for this group.
+  ROS_INFO_NAMED("tutorial", "114 End effector link: %s", move_group.getEndEffectorLink().c_str());
 
-  //// Planning to a Pose goal
-  //// ^^^^^^^^^^^^^^^^^^^^^^^
-  //// We can plan a motion for this group to a desired pose for the
-  //// end-effector.
-  //geometry_msgs::Pose target_pose1;
-  //target_pose1.orientation.w = 1.0;
-  //target_pose1.position.x = 0.28;
-  //target_pose1.position.y = -0.7;
-  //target_pose1.position.z = 1.0;
-  //move_group.setPoseTarget(target_pose1);
+  // Planning to a Pose goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^
+  // We can plan a motion for this group to a desired pose for the
+  // end-effector.
+  geometry_msgs::Pose target_pose1;
+  target_pose1.orientation.w = 1.0;
+  target_pose1.position.x = 0.30;
+  target_pose1.position.y = 0.00;
+  target_pose1.position.z = 0.00;
+  move_group.setPoseTarget(target_pose1);
+  ROS_INFO("126");
 
-  //// Now, we call the planner to compute the plan and visualize it.
-  //// Note that we are just planning, not asking move_group
-  //// to actually move the robot.
-  //moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  // Now, we call the planner to compute the plan and visualize it.
+  // Note that we are just planning, not asking move_group
+  // to actually move the robot.
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  //bool success = move_group.plan(my_plan);
+  bool success = move_group.plan(my_plan);
 
-  //ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+  ROS_INFO_NAMED("tutorial", "135 Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
 
-  //// Visualizing plans
-  //// ^^^^^^^^^^^^^^^^
-  //// We can also visualize the plan as a line with markers in Rviz.
-  //ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
-  ////visual_tools.publishAxisLabeled(target_pose1, "pose1");
-  ////visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
-  ////visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-  ////visual_tools.trigger();
-  ////visual_tools.prompt("next step");
+  // Visualizing plans
+  // ^^^^^^^^^^^^^^^^
+  // We can also visualize the plan as a line with markers in Rviz.
+  ROS_INFO_NAMED("tutorial", "140 Visualizing plan 1 as trajectory line");
+  visual_tools.publishAxisLabeled(target_pose1, "pose1");
+  visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("next step");
 
-  //// Moving to a pose goal
-  //// ^^^^^^^^^^^^^^^^^^^^^
-  ////
-  //// Moving to a pose goal is similar to the step above
-  //// except we now use the move() function. Note that
-  //// the pose goal we had set earlier is still active
-  //// and so the robot will try to move to that goal. We will
-  //// not use that function in this tutorial since it is
-  //// a blocking function and requires a controller to be active
-  //// and report success on execution of a trajectory.
+  // Moving to a pose goal
+  // ^^^^^^^^^^^^^^^^^^^^^
+  //
+  // Moving to a pose goal is similar to the step above
+  // except we now use the move() function. Note that
+  // the pose goal we had set earlier is still active
+  // and so the robot will try to move to that goal. We will
+  // not use that function in this tutorial since it is
+  // a blocking function and requires a controller to be active
+  // and report success on execution of a trajectory.
 
-  ///* Uncomment below line when working with a real robot */
-  ///* move_group.move() */
+  /* Uncomment below line when working with a real robot */
+  /* move_group.move() */
 
-  //// Planning to a joint-space goal
-  //// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  ////
-  //// Let's set a joint space goal and move towards it.  This will replace the
-  //// pose target we set above.
-  ////
-  //// To start, we'll create an pointer that references the current robot's state.
-  //// RobotState is the object that contains all the current position/velocity/acceleration data.
-  //moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-  ////
-  //// Next get the current set of joint values for the group.
-  //std::vector<double> joint_group_positions;
-  //current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+  // Planning to a joint-space goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // Let's set a joint space goal and move towards it.  This will replace the
+  // pose target we set above.
+  //
+  // To start, we'll create an pointer that references the current robot's state.
+  // RobotState is the object that contains all the current position/velocity/acceleration data.
+  moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+  //
+  // Next get the current set of joint values for the group.
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
 
-  //// Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
-  //joint_group_positions[0] = -1.0;  // radians
-  //move_group.setJointValueTarget(joint_group_positions);
+  // Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
+  joint_group_positions[0] = -1.0;  // radians
+  move_group.setJointValueTarget(joint_group_positions);
 
-  //success = move_group.plan(my_plan);
-  //ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
+  success = move_group.plan(my_plan);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
 
-  //// Visualize the plan in Rviz
-  ////visual_tools.deleteAllMarkers();
-  ////visual_tools.publishText(text_pose, "Joint Space Goal", rvt::WHITE, rvt::XLARGE);
-  ////visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-  ////visual_tools.trigger();
-  ////visual_tools.prompt("next step");
+  // Visualize the plan in Rviz
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishText(text_pose, "Joint Space Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("next step");
 
   //// Planning with Path Constraints
   //// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
