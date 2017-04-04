67c0c9ceb5cdca64ed7cb672d9f726c391c34855
Craig Christensen <crg.chrstnsn@gmail.com>
2017-04-04 07:19:48 -0500

    Changes to IK. Additions to Towers.

--- src/scorbot_ik.cpp
+++ src/scorbot_ik.cpp
@@ -23,166 +23,52 @@ ros::Publisher      elbowPub;
 ros::Publisher  wristRollPub;
 ros::Publisher wristPitchPub;
 
-void transMat(float x, float y, float z, float out[4][4])
-{
-    out[0][0] = 1;
-    out[0][1] = 0;
-    out[0][2] = 0;
-    out[0][3] = x;
-
-    out[1][0] = 0;
-    out[1][1] = 1;
-    out[1][2] = 0;
-    out[1][3] = y;
-
-    out[2][0] = 0;
-    out[2][1] = 0;
-    out[2][2] = 1;
-    out[2][3] = z;
-
-    out[3][0] = 0;
-    out[3][1] = 0;
-    out[3][2] = 0;
-    out[3][3] = 1;
-}
-
-void rollMat(float theta, float out[4][4])
-{
-    out[0][0] = 1;
-    out[0][1] = 0;
-    out[0][2] = 0;
-    out[0][3] = 0;
-
-    out[1][0] = 0;
-    out[1][1] = cos(theta);
-    out[1][2] = 0 - sin(theta);
-    out[1][3] = 0;
-
-    out[2][0] = 0;
-    out[2][1] = sin(theta);
-    out[2][2] = cos(theta);
-    out[2][3] = 0;
-
-    out[3][0] = 0;
-    out[3][1] = 0;
-    out[3][2] = 0;
-    out[3][3] = 1;
-}
-
-void pitchMat(float theta, float out[4][4])
-{
-    out[0][0] = cos(theta);
-    out[0][1] = 0;
-    out[0][2] = sin(theta);
-    out[0][3] = 0;
-
-    out[1][0] = 0;
-    out[1][1] = 1;
-    out[1][2] = 0;
-    out[1][3] = 0;
-
-    out[2][0] = 0 - sin(theta);
-    out[2][1] = 0;
-    out[2][2] = cos(theta);
-    out[2][3] = 0;
-
-    out[3][0] = 0;
-    out[3][1] = 0;
-    out[3][2] = 0;
-    out[3][3] = 1;
-}
-
-void yawMat(float theta, float out[4][4])
-{
-    out[0][0] = cos(theta);
-    out[0][1] = 0 - sin(theta);
-    out[0][2] = 0;
-    out[0][3] = 0;
-
-    out[1][0] = sin(theta);
-    out[1][1] = cos(theta);
-    out[1][2] = 0;
-    out[1][3] = 0;
-
-    out[2][0] = 0;
-    out[2][1] = 0;
-    out[2][2] = 1;
-    out[2][3] = 0;
-
-    out[3][0] = 0;
-    out[3][1] = 0;
-    out[3][2] = 0;
-    out[3][3] = 1;
-}
-
-void matMult(float mat0[4][4], float mat1[4][4], float out[4][4])
-{
-    ROS_INFO_NAMED(NODE_NAME, "############################################");
-    
-    ROS_INFO_NAMED(NODE_NAME, "_mat0_______________________________________");
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat0[0][0], mat0[0][1], mat0[0][2], mat0[0][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat0[1][0], mat0[1][1], mat0[1][2], mat0[1][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat0[2][0], mat0[2][1], mat0[2][2], mat0[2][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat0[3][0], mat0[3][1], mat0[3][2], mat0[3][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "L__________________________________________J");
-    ROS_INFO_NAMED(NODE_NAME, "_mat1_______________________________________");
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat1[0][0], mat1[0][1], mat1[0][2], mat1[0][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat1[1][0], mat1[1][1], mat1[1][2], mat1[1][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat1[2][0], mat1[2][1], mat1[2][2], mat1[2][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", mat1[3][0], mat1[3][1], mat1[3][2], mat1[3][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "L__________________________________________J");
-    for (int ox = 0; ox < 4; ox++)
-    {
-        for (int oy = 0; oy < 4; oy++)
-        {
-            out[oy][ox] = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                out[oy][ox] += mat0[oy][i] * mat1[i][ox];
-            }
-        }
-    }
-    ROS_INFO_NAMED(NODE_NAME, "_out_=_mat0_*_mat1__________________________");
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", out[0][0], out[0][1], out[0][2], out[0][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", out[1][0], out[1][1], out[1][2], out[1][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", out[2][0], out[2][1], out[2][2], out[2][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "|%f %f %f %f|", out[3][0], out[3][1], out[3][2], out[3][3]);  
-    ROS_INFO_NAMED(NODE_NAME, "L__________________________________________J");
-}
-
-void matAdd(float mat0[4][4], float mat1[4][4], float out[4][4])
-{
-    for (int x=0; x<4; x++)
-    {
-        for (int y=0; y<4; y++)
-        {
-            if (mat0[y][x] == 1)
-                mat0[y][x] = 0;
-
-            if (mat1[y][x] == 1)
-                mat1[y][x] = 0;
-
-            out[y][x] = mat0[y][x] + mat1[y][x]; 
-        }
-    }
-}
-
 bool checkAngles(float angles[5])
 {
-    ROS_INFO("0: %f - %f",  B_MIN,  B_MAX);
-    ROS_INFO("1: %f - %f",  S_MIN,  S_MAX);
-    ROS_INFO("2: %f - %f",  E_MIN,  E_MAX);
-    ROS_INFO("3: %f - %f", WP_MIN, WP_MAX);
-    ROS_INFO("4: -INF - INF\n");
-    
-    // For the scorbot er-III there is no limit for the roll.
-    if (angles[0] < B_MIN || angles[0] > B_MAX ||
-            angles[1] < S_MIN || angles[1] > B_MAX ||
-            angles[2] < E_MIN || angles[2] > E_MAX ||
-            angles[3] < WP_MIN || angles[3] > WP_MAX //||
-            //angles[4] < WR_MIN || angles[4] > WR_MIN
-            )
+    if (angles[0] < B_MIN)
+    {
+        ROS_INFO("Base angle (%f) is less than the minimum angle (%f)",  angles[0], B_MIN);
         return false;
+    }
+    if (angles[0] > B_MAX)
+    {
+        ROS_INFO("Base angle (%f) is greater than the maximum angle (%f)",  angles[0], B_MAX);
+        return false;
+    }
+
+    if (angles[1] < S_MIN)
+    {
+        ROS_INFO("Shoulder angle (%f) is less than the minimum angle (%f)",  angles[1], S_MIN);
+        return false;
+    }
+    if (angles[1] > S_MAX)
+    {
+        ROS_INFO("Shoulder angle (%f) is greater than the maximum angle (%f)",  angles[1], S_MAX);
+        return false;
+    }
+
+    if (angles[2] < E_MIN)
+    {
+        ROS_INFO("Elbow angle (%f) is less than the minimum angle (%f)",  angles[2], E_MIN);
+        return false;
+    }
+    if (angles[2] > E_MAX)
+    {
+        ROS_INFO("Elbow angle (%f) is greater than the maximum angle (%f)",  angles[2], E_MAX);
+        return false;
+    }
+    
+    if (angles[3] < WP_MIN)
+    {
+        ROS_INFO("Wrist pitch angle (%f) is less than the minimum angle (%f)",  angles[3], WP_MIN);
+        return false;
+    }
+    if (angles[3] > WP_MAX)
+    {
+        ROS_INFO("Wrist pitch angle (%f) is greater than the maximum angle (%f)",  angles[3], WP_MAX);
+        return false;
+    }
+
     return true;
 }
 
@@ -199,50 +85,80 @@ bool ik(float x, float y, float z, float R, float P, float out[5])
     }
 
     // knowns
-    float d0 = 0.349;
-    float d1 = 0.016;
+    float d0 = 0.350;
+    float d1 = 0.030;
     float d2 = 0.220;
     float d3 = 0.220;
-    float d4 = 0.045;
+    float d4 = 0.160;
 
+    ROS_INFO("x: %f, y: %f, z: %f, roll: %f, pitch: %f", x, y, z, R, P);
     float d4z = sin(P) * d4;
+    ROS_INFO("d4z: %f", d4z);
 
     // check if the wrist joint or the back of the end effector is beneath the floor.
     // the 0.004 is to account for the radius of the joint.
-    if (z - d4z - 0.004 < 0 || z - 2 * d4z - 0.004 < 0)
+    if (z + d4z - 0.004 < 0 || z + 2 * d4z < 0)
     {
-        ROS_INFO("Invalid wrist joint position");
+        ROS_INFO("Invalid wrist joint position - joint Z: %f end Z: %f", z - d4z - 0.004, z - 2 * d4z);
         return false;
     }
     
     float d4r = cos(P) * d4;
+    ROS_INFO("d4r: %f", d4r);
     float x_squared = pow(x, 2);
+    ROS_INFO("x^2: %f", x_squared);
     float y_squared = pow(y, 2);
+    ROS_INFO("y^2: %f", y_squared);
     float r = sqrt(x_squared + y_squared);
+    ROS_INFO("r: %f", r);
     float er = r - d1 - d4r;
-    float ez = z - d0 - d4z;
+    ROS_INFO("er: %f", er);
+    float ez = z + d4z - d0;
+    ROS_INFO("ez: %f", ez);
     float er_squared = pow(er, 2);
+    ROS_INFO("er^2: %f", er_squared);
     float ez_squared = pow(ez, 2);
+    ROS_INFO("ez^2: %f", ez_squared);
     float e_squared = er_squared + ez_squared;
+    ROS_INFO("e^2: %f", e_squared);
     float e = sqrt(e_squared);
+    ROS_INFO("e: %f", e);
     float d2_squared = pow(d2, 2);
+    ROS_INFO("d2^2: %f", d2_squared);
     float d3_squared = pow(d3, 2);
+    ROS_INFO("d3^2: %f", d3_squared);
     float A_top = d3_squared - d2_squared - e_squared;
+    ROS_INFO("A_top: %f", A_top);
     float A_bot = -2 * d2 * e;
+    ROS_INFO("A_bot: %f", A_bot);
     float A = acos(A_top / A_bot);
+    ROS_INFO("A: %f", A);
     float E_top = e_squared - d3_squared - d2_squared;
+    ROS_INFO("E_top: %f", E_top);
     float E_bot = -2 * d2 * d3;
-    float E = acos(E_top / E_bot) - PI;
+    ROS_INFO("E_bot: %f", E_bot);
+    float E = PI - acos(E_top / E_bot);
+    ROS_INFO("E: %f", E);
     float Ez_top = ez_squared - e_squared - er_squared;
+    ROS_INFO("Ez_top: %f", Ez_top);
     float Ez_bot = -2 * e * er;
+    ROS_INFO("Ez_bot: %f", Ez_bot);
     float Ez = acos(Ez_top / Ez_bot);
-    float S = A + Ez;
+    ROS_INFO("Ez: %f", Ez);
+    float S = A + atan2(z, x) - atan2(d3 * sin(E), d2 + d3 * cos(E));//z + d4z < z ? A + Ez : A - Ez;
+    ROS_INFO("S: %f", S);
     float Salt = (E - S);
+    ROS_INFO("Salt: %f", Salt);
     float B = atan2(y, x);
+    ROS_INFO("B: %f", B);
     float PI_div2 = PI / 2;
+    ROS_INFO("PI_div2: %f", PI_div2);
     float G = PI_div2 - Salt;
-    float Wp = PI_div2 - G + P;
+    ROS_INFO("G: %f", G);
+    float Wp =  P - (S + E);//z < d4z ? PI_div2 + G - P: PI_div2 - G + P;
+    ROS_INFO("Wp: %f", Wp);
     float Wr = R;
+    ROS_INFO("Wr: %f", Wr);
     
     if (er > d2 + d3)
     {
@@ -290,8 +206,6 @@ bool ik(float x, float y, float z, float R, float P, float out[5])
 
 void callback(const sac_msgs::Target::ConstPtr& msg)
 {
-    ROS_INFO_NAMED(NODE_NAME, "Hit");
-
     float roll  = msg->roll;
     float pitch = msg->pitch;
 
@@ -300,8 +214,9 @@ void callback(const sac_msgs::Target::ConstPtr& msg)
     float z = msg->z;
 
     float out[5];
-    ROS_INFO_NAMED(NODE_NAME, "ik call");
-    ik(x, y, z, roll, pitch, out);
+    
+    if (!ik(x, y, z, roll, pitch, out))
+        return;
 
     sac_msgs::MotorPos pos;
     pos.speed = msg->speed;
